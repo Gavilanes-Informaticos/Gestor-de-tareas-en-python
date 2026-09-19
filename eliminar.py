f9245aff9e74ff58a7a72b7ace339d8b0d7fd1ac
osc <73138388@continental.edu.pe>
2026-09-18 20:34:38 -0500

    prueba

--- eliminar.py
+++
@@ -1,6 +0,0 @@
-def eliminar_tarea(lista_tareas, indice):
-    if 0 <= indice < len(lista_tareas):
-        eliminada = lista_tareas.pop(indice)
-        print(f"✔ Tarea eliminada: '{eliminada}'")
-    else:
-        print("❌ Índice no válido o lista vacía")
